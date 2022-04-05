d1492c14e391a2564e4eadeb893a13baa2649595
steffanie <steffanieahrensedu@gmail.com>
2022-04-05 16:51:58 +0200

    Premier jour d'exercice

---
+++ 1.1.js
@@ -0,0 +1,6 @@
+let age= window.prompt("What's your age moron?")
+if (age<18){
+    console.log("You are a child get out of here")
+} else {
+    console.log("You are an adult or a liar")
+}
---
+++ 2.2.js
@@ -0,0 +1,6 @@
+let min= window.prompt("Enter a smaller number");
+let max= window.prompt("Enter a bigger number");    
+
+if(min>max){
+    console.log("I do not tolerate idiots. Bye")
+}
