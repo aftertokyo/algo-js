ad1f9d38dade9404be6ed67335294cac2dc6e101
steffanie <steffanieahrensedu@gmail.com>
2022-04-13 15:40:33 +0200

    suite

--- 4.1.js
+++ 4.1.js
@@ -1,6 +1,6 @@
 let n = prompt("enter the lenght + width of a rectangle")
 let calcSurface = (lenght,width) = {
-    return lenght + width;
+    return :lenght + width
 }
 
 console.log(calcSurface(lenght,width));
